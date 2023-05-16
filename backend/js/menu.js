1cf9dba3c1b566b3a19288dfe53642df69cb78d0
Kilian <kilian.perez@kobu-smart.co.uk>
2023-05-16 16:49:51 +0100

    Final__Test

--- backend/js/menu.js
+++ backend/js/menu.js
@@ -10,12 +10,12 @@ navIcon.onclick = function(){
         navIcon.style.background = "#fff";
         navIcon.style.border = "1px solid #33200e";
         navIcon.style.borderRadius = "200px";
-        menu.src = "/images/close.png";
+        menu.src = "../images/close.png";
     }
     else{
         navBar.style.right = "-250px";
         navIcon.style.background = "#33200e";
-        menu.src = "/images/menu-icon.png";
+        menu.src = "../images/menu-icon.png";
     }
 
 }
