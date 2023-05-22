f0d0dece1332eeae4f58498474e591c53d964283
Kilian <kilian.perez@kobu-smart.co.uk>
2023-05-22 15:27:34 +0100

    2

--- content/insert.php
+++ content/insert.php
@@ -1,55 +1,52 @@
 <?php
 if (isset($_POST['submit'])) {
-    if (isset($_POST['username']) && isset($_POST['email'])){
-            //Select data to send via POST to DB
-   $name = $_POST['username'];
-   $email = $_POST['email'];
-    
-    $host = "localhost";
-    $dbUsername = "hair_user";
-    $dbPassword = "KobuSmart@123";
-    $dbname = "hair_db";
-
-   
-    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-    if ($conn->connect_error) {
-        die('Could not connect to the database.');
-    }
-    else {
-        $Select = "SELECT email FROM subscribers WHERE email = ? LIMIT 1";
-        $Insert = "INSERT INTO subscribers(username, email) values(?, ?)";
-        $stmt = $conn->prepare($Select);
-        $stmt->bind_param("s", $email);
-        $stmt->execute();
-        $stmt->bind_result($resultEmail);
-        $stmt->store_result();
-        $stmt->fetch();
-        $rnum = $stmt->num_rows;
-        if ($rnum == 0) {
-            $stmt->close();
-            $stmt = $conn->prepare($Insert);
-            $stmt->bind_param("ssssii",$username, $email);
-            if ($stmt->execute()) {
-                echo "New record inserted sucessfully.";
-            }
-            else {
-                echo $stmt->error;
-            }
+    if (isset($_POST['username']) &&
+        isset($_POST['email'])) {
+        
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $host = "localhost";
+        $dbUsername = "hair_user";
+        $dbPassword = "KobuSmart@123";
+        $dbName = "hair_db";
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
         }
         else {
-            echo "Someone already registers using this email.";
+            $Select = "SELECT email FROM subscribers WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO subscribers(username, email) values(?, ?)";
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+            if ($rnum == 0) {
+                $stmt->close();
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssssii",$username, $email);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully.";
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
         }
-        $stmt->close();
-        $conn->close();
+    }
+    else {
+        echo "All field are required.";
+        die();
     }
 }
 else {
-    echo "All field are required.";
-    die();
-}
-
-else {
-echo "Submit button is not set";
-}
+    echo "Submit button is not set";
 }
 ?>
