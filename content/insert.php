c336d58328c4e2db60f3d18aa5be7b2a5be937f2
Kilian <kilian.perez@kobu-smart.co.uk>
2023-05-22 14:15:31 +0100

    all

--- content/insert.php
+++ content/insert.php
@@ -10,6 +10,7 @@
 
     //Connections to DB
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+   }
 //
 //    if (mysqli_connect_error()) {
  //       die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
