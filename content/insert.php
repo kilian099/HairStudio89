85a69ad389a83dc84f2684c45539fc81159504e4
Kilian <kilian.perez@kobu-smart.co.uk>
2023-05-22 14:35:36 +0100

    testing

--- content/insert.php
+++ content/insert.php
@@ -1,6 +1,6 @@
 <?php
     //Select data to send via POST to DB
-   $name = $_POST['name'];
+   $name = $_POST['username'];
    $email = $_POST['email'];
    if (!empty($name) || !empty($email)) {
     $host = "localhost";
@@ -15,16 +15,16 @@
    if (mysqli_connect_error()) {
         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
    } else {
-      $SELECT =  "SELECT email From subscribers Where email = ? Limit 1";
-       $INSERT = "INSERT Into subscribers (name, email) values (?,?)"
+       $SELECT =  "SELECT email From subscribers Where email = ? Limit 1";
+       $INSERT = "INSERT Into subscribers (username, email) values (?,?)"
 
         //Prepare Statement
       $stmt = $conn->prepare($SELECT);
       $stmt->bind_param("s", $email);
-        $stmt->execute();
+      $stmt->execute();
       $stmt->bind_result($email);
-       $stmt->store_result();
-     $rnum = $stmt->num_rows;//
+      $stmt->store_result();
+      $rnum = $stmt->num_rows;
 
         if($rnum==0) {
             $stmt->close();
@@ -34,7 +34,7 @@
             $stmt->execute();
            echo "Thanks for subscribe";
         } else {
-            echo "This email is already register, please try different email";
+           echo "This email is already register, please try different email";
         }
        $stmt->close();
         $conn->close();
