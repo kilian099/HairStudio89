23c7cc2311648fb7ac0e1c71483131d176d47bad
Kilian Perez <kilian.perez@kobu-smart.co.uk>
2023-05-23 17:16:10 +0000

    fixed_subscribers

--- content/insert.php
+++ content/insert.php
@@ -1,11 +1,11 @@
 <?php
-$name = filter_input(INPUT_POST, "name", FILTER_VALIDATE_BOOL);
-$email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_BOOL);
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-$host = "localhost";
-$dbname = "hair_db";
-$username = "hair_user";
-$password = "KobuSmart@123";
+$host = 'localhost';
+$dbname = 'hair_db';
+$username = 'hair_user';
+$password = 'KobuSmart@123';
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
@@ -14,7 +14,7 @@ if (mysqli_connect_errno()) {
 }
 
 $sql = "INSERT INTO subscribers (name, email)
-        VALUES (?, ?, ?, ?)"
+        VALUES (?, ?)";
 
 $stmt = mysqli_stmt_init($conn);
 
@@ -22,9 +22,7 @@ if ( ! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_error($conn));
 }
 
-mysqli_stmt_bind_param($stmt, "ss",
-                        $name,
-                        $email);
+mysqli_stmt_bind_param($stmt, 'ss', $name, $email);
 mysqli_stmt_execute($stmt); 
 
 echo "Thank you for subscribe, please go back to the page";
