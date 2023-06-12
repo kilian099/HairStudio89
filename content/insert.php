787351f789797955b84c9611d407cfebd339ea8b
Kilian <kilian.perez@kobu-smart.co.uk>
2023-06-12 21:20:37 +0100

    repaired_mysqli

--- content/insert.php
+++ content/insert.php
@@ -9,22 +9,38 @@ $password = 'KobuSmart@123';
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
+//Check connection
+
 if (mysqli_connect_errno()) {
-    die("Connection error: " . mysqli_connect_error());
+    die("Connection error: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO subscribers (name, email)
-        VALUES (?, ?)";
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Sanitize and validate input
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
 
-$stmt = mysqli_stmt_init($conn);
+    // Check if the combination of name and email already exists
+    $checkQuery = "SELECT * FROM subscribers WHERE name = '$name' AND email = '$email'";
+    $result = $conn->query($checkQuery);
 
-if ( ! mysqli_stmt_prepare($stmt, $sql)) {
-    die(mysqli_error($conn));
+    if ($result->num_rows > 0) {
+        echo "Value already exists in the table.";
+    } else {
+        // Value does not exist, insert it
+        $insertQuery = "INSERT INTO subscribers (name. email) VALUES ('$name', '$email')";
+
+        if ($conn->query($insertQuery) === TRUE) {
+            echo "Data inserted successfully";
+            //  To redirect form on a particular page
+            header("Location:http://127.0.0.1:5500/content/thankyou.html");
+        } else {
+            echo "Error: " . $insertQuery . "<br>" . $conn->error;
+        }
+    }
 }
 
-mysqli_stmt_bind_param($stmt, 'ss', $name, $email);
-mysqli_stmt_execute($stmt); 
-
-//  To redirect form on a particular page
-header("Location:http://127.0.0.1:5500/content/thankyou.html");
+// Close the database connection
+$conn->close();
 ?>
